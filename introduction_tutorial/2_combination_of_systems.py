7370aa9e164cee51b72d08d383f1ff71723bb874
mbero-akoko <mberoakoko@gmail.com>
2025-09-08 17:16:38 +0200

    simulation added

--- introduction_tutorial/2_combination_of_systems.py
+++ introduction_tutorial/2_combination_of_systems.py
@@ -32,10 +32,45 @@ def simple_diagram_builder():
     logger = LogVectorOutput(pendulum.get_state_output_port(), builder)
     logger.set_name("vec_output_logger")
     #
-    diagram = builder.Build()
-    diagram.set_name("diagram")
-    #
-    plot_system_graphviz(diagram)
+    diagram_ = builder.Build()
+    diagram_.set_name("diagram")
+
+    # fig: Figure = plt.figure(figsize=(16, 9))
+    return diagram_, pendulum__, controlller_, logger__
+
+def simulate_system(diagram_: Diagram,pendulum_: PendulumPlant, controller: PidController, logger_: VectorLogSink) -> None:
+    simulator = Simulator(diagram_)
+    context: Context = simulator.get_mutable_context()
+    desired_angle = np.pi / 2
+    pendulum_context_ = diagram_.GetMutableSubsystemContext(pendulum_, context)
+    print(f"\033[94m{pendulum_context_}\033[0m")
+    (
+        pendulum_context_
+        .get_mutable_continuous_state_vector()
+        .SetFromVector(
+            np.array([desired_angle + 0.1, 0.2])
+        )
+    )
+
+    (
+        diagram_
+        .get_input_port(0)
+        .FixValue(context, [desired_angle, 0])
+    )
+
+    simulator.AdvanceTo(30)
+
+    log = logger_.FindLog(simulator.get_context())
+    t = log.sample_times()
+    plt.figure(figsize=(16, 9))
+    # Plot theta.
+    plt.plot(t, log.data()[0, :], linewidth=0.7)
+    # Draw a line for the desired angle.
+    plt.plot([t[0], t[-1]], [desired_angle, desired_angle], 'g')
+    plt.xlabel('time (seconds)')
+    plt.ylabel('theta (rad)')
+    plt.title('PID Control of the Pendulum')
+    plt.tight_layout()
     plt.show()
 
 
